c531e74709c97b21f22235b9a91ba5eb9f7d5a8f
Sasidhar Pinjala <98568230+Sasidhar1456@users.noreply.github.com>
2024-11-12 15:05:27 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
-public class RWOnly {
+public class RWOnly 
+
+     private String name;
+
+    // Task 5: Implement getter and setter member functions
+
+    // Getter function to access the private variable
+    public String getName() {
+        return name;
+    }
+
+    // Setter function to set a value to the private variable
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
